24ff29d9d8d89614a4e5a027b3f4a339a2865547
fcicerelli <fcicerellitic@gmail.com>
2025-04-08 11:46:19 -0300

    refactorizando

---
+++ operadores.rb
@@ -0,0 +1,19 @@
+# Operadores
+
+suma = 10 + 5           # 15
+resta = 20 - 3          # 17
+multiplicacion = 4 * 2  # 8
+division = 12 / 3       # 4
+
+suma += 5               # suma = suma + 5
+resta -= 5              # resta = resta - 5
+es_mayor = 10 > 5       # true
+es_menor = 5 < 2        # false
+igual = "hola" == 'hola'# true
+diference = 10 == 5     # false
+otra_diferencia = 10 != 5   # true
+
+# && and
+operador_and = (10 > 5) and (5 < 2) # false
+operador_or = (10 > 5) or (5 < 2 )  # true
+operador_not = !operador_or         # false
--- variables.rb
+++ variables.rb
@@ -19,22 +19,4 @@ puts fecha_nacimiento.strftime("%d %B %Y")  # 28 April 1975
 
 puts edad < peso                            # true
 
-# Operadores
 
-suma = 10 + 5           # 15
-resta = 20 - 3          # 17
-multiplicacion = 4 * 2  # 8
-division = 12 / 3       # 4
-
-suma += 5               # suma = suma + 5
-resta -= 5              # resta = resta - 5
-es_mayor = 10 > 5       # true
-es_menor = 5 < 2        # false
-igual = "hola" == 'hola'# true
-diference = 10 == 5     # false
-otra_diferencia = 10 != 5   # true
-
-# && and
-operador_and = (10 > 5) and (5 < 2) # false
-operador_or = (10 > 5) or (5 < 2 )  # true
-operador_not = !operador_or         # false
